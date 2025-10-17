7f4ff9fe54c989b7eb0168fa894e32deadd8a688
Nguyen Manh Dung <Dung.NM213838@sis.hust.edu.vn>
2025-10-17 20:29:45 +0700

    sua add

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ void init_input(int len, char *arr) { // tao dau vao random
 		int random_number = (rand() % 9) + 1;
 
 		if (i == len - 1) {// tao so 1 o cuoi mang
-			random_number = 1; 
+			random_number = 3; 
 		}
 
 		fprintf(f, "%d", random_number);
@@ -53,9 +53,12 @@ void print_output_txt(char* result) {
 }
 
 void print_arr(char* s) {
+	printf("\n");
 	for (int i = 0; i < strlen(s); i++) {
 		printf("%d", s[i] - '0');
 	}
+		printf("\n");
+
 }
 
 
@@ -64,23 +67,28 @@ void add_1_and_div_2(char* arr, char* result) {
 	char temp[LEN + 2]; // gom (n + 1) ky tu
 	strcpy(temp, arr);
 
-
-	//add 1
-	int carry = 1;
+//add
+	int carry = 0;
 	int add_number = 1;
-	for (int i = len - 1; i >= 0 && carry; i--) {
-		int sum = (temp[i] - '0') + add_number;
-		temp[i] = (sum % 10) + '0';
-		carry = sum / 10;
+	for(int i=len -1; i>=0;i--){
+		int sum = temp[i] -'0' + add_number+carry;
+		add_number =0;
+		carry = sum/10;
+		temp[i] = sum%10 + '0';
+
+		if(!carry) {
+		break;
+		}
 	}
-	if (carry) { // them 1 o dau neu bi tran
+
+	if (carry) {
 		memmove(temp + 1, temp, len + 1);
 		//temp[0] = '1';
 		temp[0] = carry + '0';
 		len++;
 	}
 
-	//print_arr(temp);
+	print_arr(temp);
 
 	//div2
 	int j = 0;
@@ -92,7 +100,7 @@ void add_1_and_div_2(char* arr, char* result) {
 	}
 	result[j] = '\0';
 
-	// xoa so 0 o dau neu co
+	// xoa so 0 o dau neu truong hop arr[0] <2
 	int k = 0;
 	while (result[k] == '0' && result[k + 1] != '\0') k++;
 	if (k > 0) memmove(result, result + k, strlen(result + k) + 1);
@@ -111,7 +119,7 @@ int pow_2(char *arr, char* result) {
 		exit(1);
 	}
 
-	// arr * arr
+	// arr * arr theo idx -> luu vao 1 mang temp
 	for (int i = len - 1; i >= 0; i--) {
 		int digit1 = arr[i] - '0';
 		for (int j = len - 1; j >= 0; j--) {
@@ -120,7 +128,7 @@ int pow_2(char *arr, char* result) {
 		}
 	}
 
-	// Xu ly carry tu cuoi mang len dau
+	// Xu ly temp tu cuoi mang len dau -> dung phep nhan co nho de tang bien truoc do
 	for (int i = 2 * len - 1; i > 0; i--) {
 		temp[i - 1] += temp[i] / 10;
 		temp[i] %= 10;
@@ -134,7 +142,7 @@ int pow_2(char *arr, char* result) {
 		result[i - start] = temp[i] + '0';
 	}
 	result[2 * len - start] = '\0';
-	print_arr(result);
+	//print_arr(result);
 
 	free(temp);
 
@@ -159,7 +167,7 @@ int main() {
 
 
 	printf("\n\nresult \n\n");
-	print_arr(resul_2);
+	//print_arr(resul_2);
 	printf("\n");
 
 	print_output_txt(resul_2);
